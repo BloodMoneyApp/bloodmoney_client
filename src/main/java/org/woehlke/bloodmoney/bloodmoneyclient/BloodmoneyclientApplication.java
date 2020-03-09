1b6e2c5b322b74325da6e8bf28b6b4c14a59f3f4
Thomas Woehlke <thomas.woehlke@gmail.com>
2020-03-09 23:34:10 +0100

    setup

---
+++ src/main/java/org/woehlke/bloodmoney/bloodmoneyclient/BloodmoneyclientApplication.java
@@ -0,0 +1,13 @@
+package org.woehlke.bloodmoney.bloodmoneyclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodmoneyclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodmoneyclientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/woehlke/bloodmoney/bloodmoneyclient/BloodmoneyclientApplicationTests.java
@@ -0,0 +1,13 @@
+package org.woehlke.bloodmoney.bloodmoneyclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodmoneyclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
